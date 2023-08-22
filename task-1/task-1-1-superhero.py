e35ba3fbecb8afd8c0c7826c6721b150d5c3e3f7
Tirath Bhathawala <tirath.bhathawala@gmail.com>
2023-08-22 13:10:27 +0530

    Create task1 folder and make task 1.1

---
+++ task-1/task-1-1-superhero.py
@@ -0,0 +1,19 @@
+""" Task 1.1 of Synapse """
+
+jumbled_superheroes = ["DocToR_sTRAngE", "sPidERmaN", "MoON_Knight", "caPTAIN_aMERIca", "hULK"]
+indices, decoded_names = [], []
+
+for index, name in enumerate(jumbled_superheroes):
+    indices.append(index)
+    decoded_names.append(jumbled_superheroes[index].lower().replace("_", " "))
+
+name_lengths = list(map(lambda name: len(name), decoded_names))
+
+indices.sort(key= lambda index: name_lengths[index])
+
+i = 1
+
+print("Phase 5 kickoff list :")
+for index in indices:
+    print(f"{i}. {decoded_names[index].title()}")
+    i += 1
